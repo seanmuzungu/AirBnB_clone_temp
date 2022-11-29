5b0f9eb31d09ced59f6d0ee6aed3a2799aa22e95
Sean Muzungu <106771666+seanmuzungu@users.noreply.github.com>
2022-11-29 15:43:00 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,17 @@
-#!/usr/bin/python3
-"""
-__init__ dunder method for the models directory
-"""
-from models.engine.file_storage import FileStorage
 
+
+#!/usr/bin/python3
+""" FileStorage instance """
+
+
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.state import State
 
 storage = FileStorage()
 storage.reload()
