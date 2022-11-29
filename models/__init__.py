477c6e7fa22d218c1cbd25000b303687c5fd74be
seanmuzungu <seanmuzungu@gmail.com>
2022-11-29 13:15:44 +0000

    CHORE: file storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""__init__ magic method for models directory"""
-from models.engine.file_storage import FileStorage
+""" FileStorage instance """
 
+
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.state import State
 
 storage = FileStorage()
 storage.reload()
